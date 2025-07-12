0a769877d95b65802a6bfa0d5aa66c168297993b
Khalil Ghimaji <khalilghimaji@gmail.com>
2025-07-12 18:01:23 +0100

    adding models and menu views

--- little_lemon/settings.py
+++ little_lemon/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restaurant',  # Custom app for restaurant management
+    'rest_framework',  # Django REST Framework for API development
 ]
 
 MIDDLEWARE = [
@@ -73,11 +75,18 @@ WSGI_APPLICATION = 'little_lemon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+DATABASES = {   
+    'default': {   
+        'ENGINE': 'django.db.backends.mysql',   
+        'NAME': 'LittleLemon',   
+        'USER': 'root',   
+        'PASSWORD': '',   
+        'HOST': '127.0.0.1',   
+        'PORT': '3306',   
+        'OPTIONS': {   
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"   
+        }   
+    }   
 }
 
 
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('menu/', views.MenuItemView.as_view(), name='menu-list-create'),
+    path('menu/<int:pk>/', views.SingleMenuItemView.as_view(), name='menu-detail-update-delete'),
 ]
