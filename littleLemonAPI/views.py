aa2bdbb665f553e2a243ef5a830f8626752c373d
Khalil Ghimaji <khalilghimaji@gmail.com>
2025-07-13 16:48:10 +0100

    securing the api endpoints with authentication

--- littleLemonAPI/views.py
+++ littleLemonAPI/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView
+from rest_framework.permissions import IsAuthenticated
 
 from littleLemonAPI.models import Menu, Booking
 from littleLemonAPI.serializers import MenuSerializer, BookingSerializer
@@ -7,16 +8,19 @@ from littleLemonAPI.serializers import MenuSerializer, BookingSerializer
 
 # Create your views here.
 class MenuItemView(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 
 class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 
 class BookingView(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
 
